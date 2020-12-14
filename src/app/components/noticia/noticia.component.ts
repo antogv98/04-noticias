a9c0378d0ea55d24cefbe9f7a7c05f9e9447f195
Antonio Gutierrez Villegas <antogv98@gmail.com>
2020-12-14 10:57:16 +0100

    hasta mostrar tres botones

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -12,7 +12,8 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia:Article;
   @Input() indice:number;
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser,
+    private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {}
 
@@ -20,4 +21,40 @@ export class NoticiaComponent implements OnInit {
     console.log('Noticia', this.noticia.url);
     const browser = this.iab.create(this.noticia.url, '_system');
   }
+
+  async lanzarMenu(){
+
+    const actionSheet = await this.actionSheetController.create({
+      cssClass: 'action',
+      buttons: [ {
+        text: 'Compartir',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+          // this.socialSharing.share(
+          //   this.noticia.title,
+          //   this.noticia.source.name,
+          //   '',
+          //   this.noticia.url
+          // );
+        }
+      },{
+        text: 'Favorito',
+        icon: 'heart',
+        cssClass: 'iconoFavorito',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 }
